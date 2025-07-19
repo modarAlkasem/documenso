23ba9964763f742bfda0f2ce0ed75ae6fb7044fa
mudarAlkasem <modarAlkasem@gmail.com>
2025-07-19 22:08:42 +0300

    x

--- frontend/packages/email/mailer.ts
+++ frontend/packages/email/mailer.ts
@@ -1,2 +1,78 @@
 import type { Transporter } from "nodemailer";
 import { createTransport } from "nodemailer";
+
+import { MailChannelsTransport } from "./transports/mailchannels";
+import { ResendTransport } from "./transports/resend";
+
+import {
+  NEXT_PRIVATE_SMTP_TRANSPORT,
+  NEXT_PRIVATE_MAILCHANNELS_API_KEY,
+  NEXT_PRIVATE_MAILCHANNELS_END_POINT,
+  NEXT_PRIVATE_RESEND_API_KEY,
+  NEXT_PRIVATE_SMTP_HOST,
+  NEXT_PRIVATE_SMTP_APIKEY,
+  NEXT_PRIVATE_SMTP_PORT,
+  NEXT_PRIVATE_SMTP_SECURE,
+  NEXT_PRIVATE_SMTP_APIKEY_USER,
+  NEXT_PRIVATE_SMTP_UNSAFE_IGNORE_TLS,
+  NEXT_PRIVATE_SMTP_SERVICE,
+  NEXT_PRIVATE_SMTP_USERNAME,
+  NEXT_PRIVATE_SMTP_PASSWORD,
+} from "@documenso/lib/constants/app";
+
+const getTransport = (): Transporter => {
+  const transport = NEXT_PRIVATE_SMTP_TRANSPORT() ?? "smtp-auth";
+
+  if (transport === "mailchannels") {
+    return createTransport(
+      MailChannelsTransport.makeTransport({
+        apiKey: NEXT_PRIVATE_MAILCHANNELS_API_KEY(),
+        endpoint: NEXT_PRIVATE_MAILCHANNELS_END_POINT(),
+      })
+    );
+  }
+
+  if (transport === "resend") {
+    return createTransport(
+      ResendTransport.makeTransport({
+        apiKey: NEXT_PRIVATE_RESEND_API_KEY(),
+      })
+    );
+  }
+
+  if (transport === "smtp-api") {
+    if (!NEXT_PRIVATE_SMTP_APIKEY() || !NEXT_PRIVATE_SMTP_HOST()) {
+      throw new Error(
+        'SMTP API Transport required "NEXT_PRIVATE_SMTP_HOST" and "NEXT_PRIVATE_SMTP_APIKEY"'
+      );
+    }
+
+    return createTransport({
+      host: NEXT_PRIVATE_SMTP_HOST() ?? undefined,
+      port: Number(NEXT_PRIVATE_SMTP_PORT()) || 587,
+      secure: NEXT_PRIVATE_SMTP_SECURE() === "true",
+      auth: {
+        user: NEXT_PRIVATE_SMTP_APIKEY_USER() ?? "apikey",
+        pass: NEXT_PRIVATE_SMTP_APIKEY() ?? "",
+      },
+    });
+  }
+
+  return createTransport({
+    url: NEXT_PRIVATE_SMTP_HOST(),
+    port: Number(NEXT_PRIVATE_SMTP_PORT()) || 587,
+    secure: NEXT_PRIVATE_SMTP_SECURE() === "true",
+    ignoreTLS: NEXT_PRIVATE_SMTP_UNSAFE_IGNORE_TLS() === "true",
+    auth: NEXT_PRIVATE_SMTP_USERNAME()
+      ? {
+          user: NEXT_PRIVATE_SMTP_USERNAME(),
+          pass: NEXT_PRIVATE_SMTP_PASSWORD() ?? "",
+        }
+      : undefined,
+    ...(NEXT_PRIVATE_SMTP_SERVICE()
+      ? { service: NEXT_PRIVATE_SMTP_SERVICE() }
+      : {}),
+  });
+};
+
+export const mailer = getTransport();
---
+++ frontend/packages/email/transports/resend.ts
@@ -0,0 +1,142 @@
+import type { SentMessageInfo, Transport } from "nodemailer";
+import type Mail from "nodemailer/lib/mailer";
+import type MailMessage from "nodemailer/lib/mailer/mail-message";
+import { Resend } from "resend";
+import { NEXT_PRIVATE_RESEND_API_KEY } from "@documenso/lib/constants/app";
+
+export declare const RESEND_ERROR_CODES_BY_KEY: {
+  readonly missing_required_field: 422;
+  readonly invalid_access: 422;
+  readonly invalid_parameter: 422;
+  readonly invalid_region: 422;
+  readonly rate_limit_exceeded: 429;
+  readonly missing_api_key: 401;
+  readonly invalid_api_Key: 403;
+  readonly invalid_from_address: 403;
+  readonly validation_error: 403;
+  readonly invalid_idempotency_key: 403;
+  readonly invalid_idempotent_request: 403;
+  readonly concurrent_idempotent_requests: 403;
+  readonly not_found: 404;
+  readonly method_not_allowed: 405;
+  readonly application_error: 500;
+  readonly internal_server_error: 500;
+};
+
+type ResendTransportOptions = {
+  apiKey: string;
+};
+
+export class ResendTransport implements Transport<SentMessageInfo> {
+  public name = "ResendMailTransport";
+  public version = "2.0.0";
+
+  private _client: Resend;
+
+  public static makeTransport(options: Partial<ResendTransportOptions>) {
+    const { apiKey = NEXT_PRIVATE_RESEND_API_KEY() ?? "" } = options;
+    return new ResendTransport({ apiKey });
+  }
+
+  private constructor(options: ResendTransportOptions) {
+    this._client = new Resend(options.apiKey);
+  }
+
+  public send(
+    mail: MailMessage<any>,
+    callback: (err: Error | null, info: any) => void
+  ): void {
+    if (!mail.data.from || !mail.data.to) {
+      return callback(
+        new Error('Missing required fields "to" or "from" '),
+        null
+      );
+    }
+    const mailFrom = mail.data.from;
+    const mailTo = mail.data.to;
+    const mailCc = mail.data.cc;
+    const mailBcc = mail.data.bcc;
+
+    this._client.emails
+      .send({
+        subject: mail.data.subject ?? "",
+        from: this.toResendFromAddresses(mailFrom),
+        to: this.toResendAddresses(mailTo),
+        cc: this.toResendAddresses(mailCc),
+        bcc: this.toResendAddresses(mailBcc),
+        html: mail.data.html?.toString() || "",
+        text: mail.data.text?.toString() || "",
+        attachments: this.toResendAttachements(mail.data.attachments),
+      })
+      .then((res) => {
+        if (res.error) {
+          const statusCode = RESEND_ERROR_CODES_BY_KEY[res.error.name] ?? 500;
+          throw new Error(
+            `[${statusCode}]:${res.error.name} ${res.error.message}`
+          );
+        }
+        callback(null, res.data);
+      })
+      .catch((err) => {
+        callback(err, null);
+      });
+  }
+
+  public toResendAddresses(addresses: Mail.Options["to"]) {
+    if (!addresses) {
+      return [];
+    }
+
+    if (typeof addresses === "string") {
+      return [addresses];
+    }
+
+    if (Array.isArray(addresses)) {
+      return addresses.map((address) => {
+        if (typeof address === "string") {
+          return address;
+        }
+
+        return address.address;
+      });
+    }
+    return [addresses.address];
+  }
+
+  public toResendFromAddresses(addresse: Mail.Options["from"]) {
+    if (!addresse) {
+      return "";
+    }
+    if (typeof addresse === "string") {
+      return addresse;
+    }
+
+    return `${addresse.name} <${addresse.address}>`;
+  }
+
+  public toResendAttachements(attachments: Mail.Options["attachments"]) {
+    if (!attachments) {
+      return [];
+    }
+    return attachments.map((attachement) => {
+      if (!attachement.filename || !attachement.content) {
+        throw new Error("Attachment is missing filename or content");
+      }
+
+      if (typeof attachement.content === "string") {
+        return {
+          filename: attachement.filename,
+          content: Buffer.from(attachement.content),
+        };
+      }
+
+      if (attachement.content instanceof Buffer) {
+        return {
+          filename: attachement.filename,
+          content: attachement.content,
+        };
+      }
+      throw new Error("Attachment's content must be either string or buffer");
+    });
+  }
+}
