739950d2ac72bf57b6472e1ce708c44a731f92bc
mudarAlkasem <modarAlkasem@gmail.com>
2025-09-03 01:56:01 +0300

    feat:Implement Sign In page design;

--- frontend/apps/web/src/app/(unauthenticated)/signin/page.tsx
+++ frontend/apps/web/src/app/(unauthenticated)/signin/page.tsx
@@ -8,8 +8,40 @@ import {
   OIDC_PROVIDER_LABEL,
 } from "@documenso/lib/constants/auth";
 
+import { SignInForm } from "~/components/forms/signin-form";
+
 export const metadata: Metadata = {
   title: "Sign In",
 };
 
-export default async function SignInPage() {}
+export default async function SignInPage() {
+  const NEXT_PUBLIC_DISABLE_SIGNUP = env("NEXT_PUBLIC_DISABLE_SIGNUP");
+
+  return (
+    <div className="w-screen max-w-lg px-4">
+      <div className="border border-border dark:bg-background bg-neutral-100 p-6 z-10 rounded-xl">
+        <h1 className="text-2xl font-semibold"> Sign in to your account</h1>
+        <p className="text-muted-foreground mt-2 text-sm">
+          Welcome back, we are lucky to have you.
+        </p>
+        <hr className="my-4 -mx-6" />
+        <SignInForm
+          isGoogleSSOEnabled={Boolean(IS_GOOGLE_SSO_ENABLED) ?? true}
+          isOIDCSSOEnabled={Boolean(IS_OIDC_SSO_ENABLED) ?? false}
+          oidcProviderLabel={OIDC_PROVIDER_LABEL}
+        />
+        {NEXT_PUBLIC_DISABLE_SIGNUP !== "true" && (
+          <p className="text-muted-foreground mt-6 text-center text-sm">
+            Don't have an account?{" "}
+            <Link
+              href="/signup"
+              className="transition text-documenso-700 duration-200 hover:opacity-70"
+            >
+              Sign up
+            </Link>
+          </p>
+        )}
+      </div>
+    </div>
+  );
+}
--- frontend/apps/web/src/components/forms/signin-form.tsx
+++ frontend/apps/web/src/components/forms/signin-form.tsx
@@ -37,3 +37,159 @@ import {
 import { Input } from "@documenso/ui/primitives/input";
 import { PasswordInput } from "@documenso/ui/primitives/password-input";
 import { toast } from "@documenso/ui/primitives/use-toaste";
+
+const ERROR_MESSAGES: Partial<Record<keyof typeof ErrorCode, string>> = {
+  [ErrorCode.CREDENTIALS_NOT_FOUND]:
+    "The email or password provided is incorrect",
+  [ErrorCode.INCORRECT_EMAIL_PASSWORD]:
+    "The email or password provided is incorrect",
+  [ErrorCode.USER_MISSING_PASSWORD]:
+    "This account appears to be using a social login method, please sign in using that method.",
+  [ErrorCode.INCORRECT_TWO_FACTOR_CODE]:
+    "The two-factor authentication code is incorrect.",
+  [ErrorCode.INCORRECT_TWO_FACTOR_BACKUP_CODE]:
+    "The backup code provided is incorrect.",
+  [ErrorCode.UNVERIFIED_EMAIL]:
+    "This account has not been verified. please verify your account before signing in.",
+  [ErrorCode.ACCOUNT_DISABLED]:
+    "This account has been disabled. Please contact support.",
+};
+
+const TwoFactorEnabledErrorCode = ErrorCode.TWO_FACTOR_MISSING_CREDENTIALS;
+
+const LOGIN_REDIRECT_PATH = "/documents";
+
+export const ZSignInFormSchema = z.object({
+  email: z.string().email().min(1),
+  password: ZCurrentPasswordSchema,
+  totpCode: z.string().trim().optional(),
+  backupCode: z.string().trim().optional(),
+});
+
+export type TSignInFormSchema = z.infer<typeof ZSignInFormSchema>;
+
+export type SignInFormProps = {
+  className?: string;
+  initialEmail?: string;
+  isGoogleSSOEnabled?: boolean;
+  isOIDCSSOEnabled?: boolean;
+  oidcProviderLabel?: string;
+  returnTo?: string;
+};
+
+export const SignInForm = ({
+  className,
+  initialEmail,
+  isGoogleSSOEnabled = false,
+  isOIDCSSOEnabled = false,
+  oidcProviderLabel,
+  returnTo,
+}: SignInFormProps) => {
+  const router = useRouter();
+
+  const isPasskeyEnabled = false;
+  const callbackUrl = useMemo(() => {
+    if (typeof window === "undefined") {
+      return LOGIN_REDIRECT_PATH;
+    }
+
+    let url = new URL(returnTo || LOGIN_REDIRECT_PATH, window.location.origin);
+
+    if (url.origin !== window.location.origin) {
+      url = new URL(window.location.origin, LOGIN_REDIRECT_PATH);
+    }
+
+    return url.toString();
+  }, [returnTo]);
+
+  const form = useForm<TSignInFormSchema>({
+    values: {
+      email: initialEmail ?? "",
+      password: "",
+      totpCode: "",
+      backupCode: "",
+    },
+    resolver: zodResolver(ZSignInFormSchema),
+  });
+
+  const isSubmitting = form.formState.isSubmitting;
+
+  return (
+    <Form {...form}>
+      <form
+        className={cn("flex flex-col gap-y-4 w-full", className)}
+        onSubmit={() => console.log("Form has been submitted!")}
+      >
+        <fieldset
+          className="flex flex-col gap-y-4 w-full"
+          disabled={isSubmitting}
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel> Email</FormLabel>
+                <FormControl>
+                  <Input type="email" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="password"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel> Password</FormLabel>
+                <FormControl>
+                  <Input type="password" {...field} />
+                </FormControl>
+                <FormMessage />
+                <p className="mt-2 text-right">
+                  <Link
+                    href="/forgot-password"
+                    className="text-muted-foreground text-sm transition hover:opacity-70"
+                  >
+                    Forgot your password?
+                  </Link>
+                </p>
+              </FormItem>
+            )}
+          />
+          <Button
+            type="submit"
+            size="lg"
+            loading={isSubmitting}
+            className="dark:bg-documenso dark:hover:opacity-90"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "Signing in..." : "Sign In"}
+          </Button>
+        </fieldset>
+        {(isGoogleSSOEnabled || isOIDCSSOEnabled || isPasskeyEnabled) && (
+          <div className="relative  flex items-center justify-center gap-x-4 py-2 uppercase text-xs">
+            <div className="bg-border flex-1 h-px" />
+            <span className="text-muted-foreground bg-transparent">
+              Or continue with
+            </span>
+            <div className="bg-border flex-1 h-px" />
+          </div>
+        )}
+        {isGoogleSSOEnabled && (
+          <Button
+            type="button"
+            size="lg"
+            variant="outline"
+            className="bg-background text-muted-foreground border"
+            disabled={isSubmitting}
+          >
+            <FcGoogle className="mr-2 h-5 w-5" />
+            Google
+          </Button>
+        )}
+      </form>
+    </Form>
+  );
+};
