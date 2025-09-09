5ca34eb7d8285b61f93f747a69783726c558dfc9
mudarAlkasem <modarAlkasem@gmail.com>
2025-09-10 01:06:03 +0300

    feat: Implement function to send Forgot Password Email;

--- frontend/packages/lib/server-only/auth/send-forgot-password.ts
+++ frontend/packages/lib/server-only/auth/send-forgot-password.ts
@@ -3,4 +3,55 @@ import { createElement } from "react";
 import { mailer } from "@documenso/email/mailer";
 
 import { renderEmailWithI18N } from "../../utils/render-email-with-i18n";
-import { NEXT_PUBLIC_WEBAPP_URL } from "../../constants/app";
+import {
+  NEXT_PUBLIC_WEBAPP_URL,
+  NEXT_PRIVATE_SMTP_FROM_ADDRESS,
+  NEXT_PRIVATE_SMTP_FROM_NAME,
+} from "../../constants/app";
+import { ForgotPasswordTemplate } from "@documenso/email/templates/forgot-password";
+import { getUserByUniqueField } from "../../api/users/fetchers";
+
+export type sendForgotPasswordOptions = {
+  userId: number;
+};
+
+export const sendForgotPassword = async ({
+  userId,
+}: sendForgotPasswordOptions) => {
+  const user = await getUserByUniqueField({
+    field: "id",
+    value: userId,
+  });
+
+  const assetBaseUrl = NEXT_PUBLIC_WEBAPP_URL() ?? "http://localhost:3000";
+
+  const token = "Test";
+  const resetPasswordLink = `${assetBaseUrl}/reset-password/${token}`;
+
+  const template = createElement(ForgotPasswordTemplate, {
+    assetBaseUrl,
+    resetPasswordLink,
+  });
+
+  const [html, text] = await Promise.all([
+    renderEmailWithI18N(template),
+    renderEmailWithI18N(template, {
+      plainText: true,
+    }),
+  ]);
+
+  return await mailer.sendMail({
+    to: {
+      address: user.email,
+      name: user.name,
+    },
+    from: {
+      address:
+        NEXT_PRIVATE_SMTP_FROM_ADDRESS() ?? "noreply@documenso-clone.com'",
+      name: NEXT_PRIVATE_SMTP_FROM_NAME() ?? "Documenso-Clone",
+    },
+    subject: "Forgot Password",
+    text,
+    html,
+  });
+};
--- frontend/packages/lib/server-only/user/forgot-password.ts
+++ frontend/packages/lib/server-only/user/forgot-password.ts
@@ -1,4 +1,5 @@
 import { TForgotPasswordSchema } from "@documenso/trpc/server/profile-router/schema";
+import { sendForgotPassword } from "../auth/send-forgot-password";
 
 export const forgotPassword = async ({ email }: TForgotPasswordSchema) => {
   return "test";
